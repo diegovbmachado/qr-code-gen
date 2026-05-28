df65084020d4120a5d8647ed297866d4c8802e43
diego <diegovbmachado@gmail.com>
2026-05-28 16:23:25 -0300

    sem alteracao ate 49-30

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,131 @@
+"use client";
+import {QRCodeCanvas} from 'qrcode.react';
+import Image from "next/image";
+import { FaUpload } from "react-icons/fa";
 
 
 export default function Home() {
   return (
-    <div>
-      <h1>QR Code Generator</h1>
-      <p>Welcome to the QR Code Generator!</p>
-    </div>
+    <main className="container">
+      <section className="title-content">
+        <h1 className="page-title">
+          Gere e customize 
+          QR Codes <span>dinâmicos</span>
+        </h1>
+        <Image
+        src="/arrow.svg"
+        alt="detail" 
+        width={150} 
+        height={150} 
+        className="arrow-detail"
+         />
+      </section>
+      <section className="featuresqr-code-container">
+          <div className="qr-code">
+            <div className="link-input">
+             <label htmlFor="link">
+              Digite seu link
+             </label>
+        <input 
+              type="text" 
+              id="link" 
+              placeholder="Seu link aqui"
+               />
+        </div>
+
+        <div className="qr-code-preview">
+    <p>
+    Qr Code Preview
+    </p>
+    <QRCodeCanvas
+  value={"https://diegovbmachado.github.io/Landing_page_final/"}
+  title={"https://diegovbmachado.github.io/Landing_page_final/"}
+  size={200}
+  bgColor={"#ffffff"}
+  fgColor={"#000000"}
+  imageSettings={{
+    src: "https://static.zpao.com/favicon.png",
+    x: undefined,
+    y: undefined,
+    height: 24,
+    width: 24,
+    opacity: 1,
+    excavate: true,
+  }}
+/>
+              </div>
+              </div>
+        <div className="qr-code-customize">
+            <div className="customization-comtainer">
+              <h3>
+                Cores
+              </h3>
+              <div className='input-container colors'>
+                <div className="input-box">
+                  <label htmlFor="fgColor">
+                    Cor principal
+                    </label>
+                    <input
+                    type="color"
+                    className='input-color' 
+                    id='fgColor'
+                    />
+                </div>
+                <div className="input-box">
+                  <label htmlFor="bgColor">
+                    Cor de fundo
+                    </label>
+                    <input
+                    type="color"
+                    className='input-color' 
+                    id='bgColor'
+                    />
+                </div>
+              </div>
+            </div>
+            <div className="customization-comtainer">
+               <h3>
+                Logo
+              </h3>
+
+              <div className='input-container '>
+                <div className="input-box">
+                  <label htmlFor="logo">
+                    Insira seu logo
+                    </label>
+                    <input
+                    type="file"
+                    className='input-file' 
+                    id='logo'
+                    accept="image/*"
+                    />
+                       <button className='input-file-button'>
+                    <FaUpload />
+                          Escolher arquivo
+                          </button>
+                     
+                </div>
+                <div className="input-box">
+                  <label htmlFor="logoSize">
+                    Tamanho da logo
+                    </label>
+                  <select 
+                  name="logoSize" 
+                  id="logoSize" 
+                  >
+<option value="24">24px x 24px</option>
+<option value="38">38px x 38px</option>
+<option value="50">50px x 50px</option>
+                  </select>
+                </div>
+                </div>
+            </div>
+            <button className="download-button">
+              Baixar QR Code
+            </button>
+        </div>
+
+      </section>
+    </main>
   );
 }
